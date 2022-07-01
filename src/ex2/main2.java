fc502efdf6c362963d13e7d38f5055d52a891ddf
Marco Gomes <105744770+marcuugomes@users.noreply.github.com>
2022-07-01 14:13:45 +0100

    ex2

--- src/ex2/main2.java
+++ src/ex2/main2.java
@@ -1,19 +1,43 @@
 package ex2;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main2 {
     public static void main(String[] args) throws IOException {
 
         Turma turma = new Turma();
-        // Escreve p/ ficheiro e apaga do array
+
+        /*
         System.out.println("Antes de escrever p/ ficheiro");
         turma.listaTodosAlunos();
-        turma.escreverAluno(1);
+        // Escreve p/ ficheiro
+        turma.escreverAlunoFicheiro(1);
         System.out.println("Depois de escrever p/ o ficheiro");
         turma.listaTodosAlunos();
+
+        // Lê de um ficheiro
         System.out.println("Le de um ficheiro");
-        turma.lerAluno("Aluno2");
+        turma.lerAlunoFicheiro("Aluno2");
         turma.listaTodosAlunos();
+        */
+
+        // Pedir ao utilizador os valores
+
+        /*
+        System.out.println("Introduza o número mecanográfico: ");
+         /*
+        Scanner leitor = new Scanner(System.in);
+        int nMec = leitor.nextInt();
+        System.out.println("Introduza o nome do aluno: ");
+        String nome = leitor.next();
+        turma.inscreveAluno(nMec,nome);
+        turma.listaTodosAlunos();
+        */
+
+        turma.backupTurma();
+
+
+
     }
 }
